987610dfa4cee36641e8a3b1cbabe2daff629b73
c3623714 <a.nuhhar2268@student.leedsbeckett.ac.uk>
2023-11-22 22:43:09 +0000

    Create an Eloquent Model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,39 +1,11 @@
-<?php 
-    namespace App\Models;
+<?php
 
-    class Listing {
-        public static function all(){
-            //This is come from database, but just for now
-            //lets go ahead and return values from wep.php (listing)
-            return  [
-                'listing' => [
-                    'id' => 1,
-                    'title' => 'Listing one',
-                    'description' => 'For your business, too, your customers’ wins are your wins. 
-                    If you’re a small retailer, this is true not just with your in-store customer service,
-                     but also your ecommerce website’s overall UX, its check-out flow, 
-                     your online return options, and your product descriptions.'
-                ],
-                [
-                    'id' => 2,
-                    'title' => 'Listing 2',
-                    'description' => 'For your business, too, your customers’ wins are your wins. 
-                    If you’re a small retailer, this is true not just with your in-store customer service,
-                     but also your ecommerce website’s overall UX, its check-out flow, 
-                     your online return options, and your product descriptions.'
-                ]
-    
-                ];
-        }
+namespace App\Models;
 
-        public static function find($id){
-            $listings = self::all();
-            foreach($listings as $listing){
-                // if current listing == to id that passed in to find
-                if($listing['id'] == $id){
-                    return $listing;
-                }
-            }
-        }
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
+class Listing extends Model
+{
+    use HasFactory;
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,36 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
          \App\Models\User::factory(5)->create();
+         Listing::create(
+            [
+                'title' => 'Larval Senior Developet',
+                'tags' => 'laraval java script',
+                'company' => 'Apple',
+                'location' => 'Usa',
+                'email' => 'usaapple@gmail.com',
+                'website' => 'Https://www.apple.com',
+                'description' => 'For your business, too, your customers’ wins are your wins. 
+                If you’re a small retailer, this is true not just with your in-store customer service,
+                 but also your ecommerce website’s overall UX, its check-out flow, 
+                 your online return options, and your product descriptions.'
+            ]
+         );
+
+         Listing::create(
+            [
+                'title' => 'Full Stack Engineer',
+                 'tags' => 'laraval PHP',
+                'company' => 'Google',
+                'location' => 'Usa',
+                'email' => 'google@gmail.com',
+                'website' => 'Https://www.google.com',
+                'description' => 'For your business, too, your customers’ wins are your wins. 
+                If you’re a small retailer, this is true not just with your in-store customer service,
+                 but also your ecommerce website’s overall UX, its check-out flow, 
+                 your online return options, and your product descriptions.'
+            ]
+         );
+
          //\App\Models\Listings::factory(5)->create();
 
         // \App\Models\User::factory()->create([
