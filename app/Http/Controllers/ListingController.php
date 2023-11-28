b7cb91abfec75e730ae8377fe0aca926a4b8a2b0
c3623714 <a.nuhhar2268@student.leedsbeckett.ac.uk>
2023-11-28 13:40:37 +0000

    Method Naming

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -9,7 +9,7 @@ class ListingController extends Controller
 {
     // Show all listing 
     public function index(){
-        return view('listings', [
+        return view('listings.index', [
             'listings' => Listing::all()
         ]);
 
@@ -17,7 +17,7 @@ class ListingController extends Controller
 
     // Show Single listing
     public function show(Listing $listing){
-        return view('listing', [
+        return view('listings.show', [
             'listing' => $listing
         ]);
     }
